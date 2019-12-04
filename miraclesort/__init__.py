2cb0db19c30e95242e26dcaaeaeb0a067f3e10c9
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2019-12-04 17:38:57 +0100

    Added miraclesort code

---
+++ miraclesort/__init__.py
@@ -0,0 +1,1 @@
+from miraclesort._miraclesort import miraclesort
---
+++ miraclesort/_miraclesort.py
@@ -0,0 +1,11 @@
+def _is_sorted(lst: list) -> bool:
+    for i in range(len(lst) - 1):
+        if lst[i] > lst[i + 1]:
+            return False
+    return True
+
+
+def miraclesort(lst: list) -> list:
+    while not _is_sorted(lst):
+        pass
+    return lst
